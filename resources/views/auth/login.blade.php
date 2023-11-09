efcdfb987fd4306f2d318c278834f005a3258ac9
Md Fahim Hossain <alifhossain174@gmail.com>
2023-11-10 01:07:03 +0600

    Updating Code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,97 +1,124 @@
 @extends('layouts.app')
 
+@section('header_css')
+    <style>
+        a.ghost {
+            border-radius: 20px;
+            border: 1px solid #FFFFFF;
+            background-color: transparent;
+            color: #FFFFFF;
+            font-size: 12px;
+            font-weight: bold;
+            padding: 12px 45px;
+            letter-spacing: 1px;
+            text-transform: uppercase;
+            transition: transform 80ms ease-in;
+            cursor: pointer;
+            text-decoration: none;
+        }
+    </style>
+@endsection
+
 @section('content')
+    <div class="container" id="container" style="height: 550px !important">
+        <div class="form-container sign-up-container">
 
-<div class="container" id="container">
-    <div class="form-container sign-up-container">
-        <form method="POST" action="{{ route('register') }}" onsubmit="validateForm()">
-            @csrf
-            <img src="{{url('frontend_assets')}}/images/logo-green.png" width="100px">
-            <h1 class="anotherh1">Create Account</h1>
-            <span>to use your business email for registration</span>
+            {{-- <form method="POST" action="{{ route('register') }}" onsubmit="validateForm()">
+                @csrf
+                <img src="{{ url('frontend_assets') }}/images/logo-green.png" width="100px">
+                <h1 class="anotherh1">Create Account</h1>
+                <span>to use your business email for registration</span>
 
-            <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required
-            autocomplete="name" autofocus placeholder="Name" />
-            @error('name')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
+                <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name"
+                    value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name" />
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
 
-            <input id="email" type="email" onkeyup="checkBusinessEmail(this.value)" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required
-            autocomplete="email" autofocus placeholder="Business Email" />
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
+                <input id="email" type="email" onkeyup="checkBusinessEmail(this.value)"
+                    class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required
+                    autocomplete="email" autofocus placeholder="Business Email" />
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
 
-            <input id="password" type="password" class="@error('password') is-invalid @enderror register_password" name="password" required autocomplete="current-password" placeholder="Password" />
-            @error('password')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
+                <input id="password" type="password" class="@error('password') is-invalid @enderror register_password"
+                    name="password" required autocomplete="current-password" placeholder="Password" />
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
 
-            <input id="password-confirm" class="register_confirm_password" type="password" placeholder="Confirm Password" name="password_confirmation" required autocomplete="new-password" required>
+                <input id="password-confirm" class="register_confirm_password" type="password"
+                    placeholder="Confirm Password" name="password_confirmation" required autocomplete="new-password"
+                    required>
 
-            <a href="javascript:void(0)" onclick="validateForm()" class="register_form_validaion" class="mt-2">Sign Up</a>
-            <button type="submit" id="resgiter_submit_btn" class="mt-2 d-none">Sign Up</button>
-        </form>
+                <a href="javascript:void(0)" onclick="validateForm()" class="register_form_validaion" class="mt-2">Sign
+                    Up</a>
+                <button type="submit" id="resgiter_submit_btn" class="mt-2 d-none">Sign Up</button>
+            </form> --}}
 
-    </div>
-    <div class="form-container sign-in-container">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <img src="{{url('frontend_assets')}}/images/logo-green.png" width="100px">
-            <h1 class="anotherh1">Sign In</h1>
-            <span>to use your account</span>
+        </div>
+        <div class="form-container sign-in-container">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <img src="{{ url('frontend_assets') }}/images/logo-green.png" width="100px">
+                <h1 class="anotherh1">Sign In</h1>
+                <span>to use your account</span>
 
-            <input type="email" id="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus class="@error('email') is-invalid @enderror" placeholder="Business Email" />
-            @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                <input type="email" id="email" name="email" value="{{ old('email') }}" required
+                    autocomplete="email" autofocus class="@error('email') is-invalid @enderror"
+                    placeholder="Business Email" />
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
 
-            <input id="password" type="password" name="password" class="@error('password') is-invalid @enderror" placeholder="Password" required autocomplete="current-password"/>
-            @error('password')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
+                <input id="password" type="password" name="password" class="@error('password') is-invalid @enderror"
+                    placeholder="Password" required autocomplete="current-password" />
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
 
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
-            @endif
+                @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                @endif
 
-            <button type="submit">Sign In</button>
-        </form>
-    </div>
-    <div class="overlay-container">
-        <div class="overlay">
-            <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
-                <p>To keep connected with us please login with your business info</p>
-                <button class="ghost" id="signIn">Sign In</button>
-            </div>
-            <div class="overlay-panel overlay-right">
-                <h1>Welcome to TestTalents!</h1>
-                <p>Enter your business details and start journey with us</p>
-                <button class="ghost" id="signUp">Sign Up</button>
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your business info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Welcome to TestTalents!</h1>
+                    <p>Enter your business details and start journey with us</p>
+                    {{-- <button class="ghost" id="signUp">Sign Up</button> --}}
+                    <a href="{{ url('register') }}" class="ghost">Sign Up</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
 
 
-@section("footer_js")
+@section('footer_js')
     <script>
-        function checkBusinessEmail(value){
+        function checkBusinessEmail(value) {
             let str = value;
-            if(str.includes("gmail") == true || str.includes("yahoo") == true || str.includes("outlook") == true){
+            if (str.includes("gmail") == true || str.includes("yahoo") == true || str.includes("outlook") == true) {
                 toastr.error("Only Business Email are Allowed");
                 $("#email").val("");
             }
@@ -101,11 +128,10 @@
             var regConfirmPass = $(".register_confirm_password").val();
             var regPass = $(".register_password").val();
 
-            if(regConfirmPass != regPass){
+            if (regConfirmPass != regPass) {
                 toastr.error("Password & Confirm Password is not matching");
                 return false;
-            }
-            else {
+            } else {
                 document.getElementById("resgiter_submit_btn").click();
             }
         }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,17 +14,19 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="{{url('frontend_assets')}}/images/favicon.ico">
+    <link rel="icon" href="{{ url('frontend_assets') }}/images/favicon.ico">
     <title>TestTalents</title>
-    <link rel="stylesheet" href="{{url('frontend_assets')}}/css/all.min.css">
-    <link rel="stylesheet" href="{{url('frontend_assets')}}/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{url('frontend_assets')}}/css/animate.css">
-    <link rel="stylesheet" href="{{url('frontend_assets')}}/css/slick.css">
-    <link rel="stylesheet" href="{{url('frontend_assets')}}/css/login.css">
-    <link rel="stylesheet" href="{{url('frontend_assets')}}/css/toastr.min.css">
+    <link rel="stylesheet" href="{{ url('frontend_assets') }}/css/all.min.css">
+    <link rel="stylesheet" href="{{ url('frontend_assets') }}/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ url('frontend_assets') }}/css/animate.css">
+    <link rel="stylesheet" href="{{ url('frontend_assets') }}/css/slick.css">
+    <link rel="stylesheet" href="{{ url('frontend_assets') }}/css/login.css">
+    <link rel="stylesheet" href="{{ url('frontend_assets') }}/css/toastr.min.css">
+
+    @yield('header_css')
 
     <style>
-        .invalid-feedback{
+        .invalid-feedback {
             display: block
         }
     </style>
@@ -34,20 +36,20 @@
 
     @yield('content')
 
-    <script src="{{url('frontend_assets')}}/js/jquery-1.12.4.min.js"></script>
-    <script src="{{url('frontend_assets')}}/js/popper.min.js"></script>
-    <script src="{{url('frontend_assets')}}/js/bootstrap.min.js"></script>
-    <script src="{{url('frontend_assets')}}/js/slick.min.js"></script>
-    <script src="{{url('frontend_assets')}}/js/waypoints.min.js"></script>
-    <script src="{{url('frontend_assets')}}/js/wow.min.js"></script>
-    <script src="{{url('frontend_assets')}}/js/jquery.counterup.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/jquery-1.12.4.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/popper.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/bootstrap.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/slick.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/waypoints.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/wow.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/jquery.counterup.min.js"></script>
     {{--  <script src="{{url('frontend_assets')}}/js/custom.js"></script>  --}}
     <script src="https://kit.fontawesome.com/c218529370.js"></script>
-    <script src="{{url('frontend_assets')}}/js/login.js"></script>
-    <script src="{{url('frontend_assets')}}/js/toastr.min.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/login.js"></script>
+    <script src="{{ url('frontend_assets') }}/js/toastr.min.js"></script>
     {!! Toastr::message() !!}
 
-    @yield("footer_js")
+    @yield('footer_js')
 </body>
 
 </html>
